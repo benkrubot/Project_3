6ff0070eaa4ccf2dad42f10992d84e9b6bfacf34
benkrubot <benjaminkruskamp@gmail.com>
2020-10-19 21:10:27 -0700

    Simple calculator with gui that displays result of addition and subtraction.

--- Project_3.py
+++ Project_3.py
@@ -9,86 +9,72 @@ class calc():
     def __init__(self):
         self.root = Tk()
         self.root.title("Simple Calculator")
-        self.root.geometry("360x260")
 
-        self.resultwindow = Entry(self.root)
-        self.resultwindow.grid(row=0, column=0, columnspan=6)
-        self.resultwindow.config(font=("Roboto", 18))
+        self.result = Entry(self.root, width = 35, borderwidth = 5)
+        self.result.grid(row=0, column=0, columnspan=6, padx = 10, pady = 10)
 
         # buttons
-        self.button1 = Button(self.root, text="1", width=5, command=lambda: self.ins('1'))
-        self.button1.grid(row=1, column=0, padx=5, pady=5)
-        self.button1.config(font=("Roboto", 18))
+        self.b1 = Button(self.root, text="1", width=5, command=lambda: self.button_click('1'))
+        self.b1.grid(row=3, column=0, padx=5, pady=5)
 
-        self.button2 = Button(self.root, text="2", width=5, command=lambda: self.ins('2'))
-        self.button2.grid(row=1, column=1, padx=5, pady=5)
-        self.button2.config(font=("Roboto", 18))
+        self.b2 = Button(self.root, text="2", width=5, command=lambda: self.button_click('2'))
+        self.b2.grid(row=3, column=1, padx=5, pady=5)
 
-        self.button3 = Button(self.root, text="3", width=5, command=lambda: self.ins('3'))
-        self.button3.grid(row=1, column=2, padx=5, pady=5)
-        self.button3.config(font=("Roboto", 18))
+        self.b3 = Button(self.root, text="3", width=5, command=lambda: self.button_click('3'))
+        self.b3.grid(row=3, column=2, padx=5, pady=5)
 
-        self.button4 = Button(self.root, text="4", width=5, command=lambda: self.ins('4'))
-        self.button4.grid(row=2, column=0, padx=5, pady=5)
-        self.button4.config(font=("Roboto", 18))
+        self.b4 = Button(self.root, text="4", width=5, command=lambda: self.button_click('4'))
+        self.b4.grid(row=2, column=0, padx=5, pady=5)
 
-        self.button5 = Button(self.root, text="5", width=5, command=lambda: self.ins('5'))
-        self.button5.grid(row=2, column=1, padx=5, pady=5)
-        self.button5.config(font=("Roboto", 18))
+        self.b5 = Button(self.root, text="5", width=5, command=lambda: self.button_click('5'))
+        self.b5.grid(row=2, column=1, padx=5, pady=5)
 
-        self.button6 = Button(self.root, text="6", width=5, command=lambda: self.ins('6'))
-        self.button6.grid(row=2, column=2, padx=5, pady=5)
-        self.button6.config(font=("Roboto", 18))
+        self.b6 = Button(self.root, text="6", width=5, command=lambda: self.button_click('6'))
+        self.b6.grid(row=2, column=2, padx=5, pady=5)
 
-        self.button7 = Button(self.root, text="7", width=5, command=lambda: self.ins('7'))
-        self.button7.grid(row=3, column=0, padx=5, pady=5)
-        self.button7.config(font=("Roboto", 18))
+        self.b7 = Button(self.root, text="7", width=5, command=lambda: self.button_click('7'))
+        self.b7.grid(row=1, column=0, padx=5, pady=5)
 
-        self.button8 = Button(self.root, text="8", width=5, command=lambda: self.ins('8'))
-        self.button8.grid(row=3, column=1, padx=5, pady=5)
-        self.button8.config(font=("Roboto", 18))
+        self.b8 = Button(self.root, text="8", width=5, command=lambda: self.button_click('8'))
+        self.b8.grid(row=1, column=1, padx=5, pady=5)
 
-        self.button9 = Button(self.root, text="9", width=5, command=lambda: self.ins('9'))
-        self.button9.grid(row=3, column=2, padx=5, pady=5)
-        self.button9.config(font=("Roboto", 18))
+        self.b9 = Button(self.root, text="9", width=5, command=lambda: self.button_click('9'))
+        self.b9.grid(row=1, column=2, padx=5, pady=5)
 
-        self.button0 = Button(self.root, text="0", width=5, command=lambda: self.ins('0'))
-        self.button0.grid(row=4, column=0, padx=5, pady=5)
-        self.button0.config(font=("Roboto", 18))
+        self.b0 = Button(self.root, text="0", width=5, command=lambda: self.button_click('0'))
+        self.b0.grid(row=4, column=0, padx=5, pady=5)
 
         # basic operations
-        self.buttonplus = Button(self.root, text="+", width=5, command=lambda: self.ins('+'))
-        self.buttonplus.grid(row=1, column=3, padx=5, pady=5)
-        self.buttonplus.config(font=("Roboto", 18))
+        self.bplus = Button(self.root, text="+", width=5, command=lambda: self.button_click('+'))
+        self.bplus.grid(row=1, column=3, padx=5, pady=5)
 
-        self.buttonminus = Button(self.root, text="-", width=5, command=lambda: self.ins('-'))
-        self.buttonminus.grid(row=2, column=3, padx=5, pady=5)
-        self.buttonminus.config(font=("Roboto", 18))
+        self.bminus = Button(self.root, text="-", width=5, command=lambda: self.button_click('-'))
+        self.bminus.grid(row=2, column=3, padx=5, pady=5)
 
-        self.buttondelete = Button(self.root, text="Del", width=5, command=lambda: self.delete())
-        self.buttondelete.grid(row=3, column=3, padx=5, pady=5)
-        self.buttondelete.config(font=("Roboto", 18))
+        self.bdelete = Button(self.root, text="Del", width=5, command=lambda: self.delete())
+        self.bdelete.grid(row=3, column=3, padx=5, pady=5)
 
-        self.buttonresult = Button(self.root, text="=", width=5, command=lambda: self.calculate())
-        self.buttonresult.grid(row=4, column=3, padx=5, pady=5)
-        self.buttonresult.config(font=("Roboto", 18))
+        self.bresult = Button(self.root, text="=", width=5, command=lambda: self.calculate())
+        self.bresult.grid(row=4, column=3, padx=5, pady=5)
 
         self.root.mainloop()
 
-    def ins(self, val):
-        self.resultwindow.insert(END, val)
+    def button_click(self, number):
+        current = self.result.get()
+        self.result.delete(0, END)
+        self.result.insert(0, current + number)
 
     def delete(self):
-        x = self.resultwindow.get()
-        self.resultwindow.delete(0, 'end')
+        x = self.result.get()
+        self.result.delete(0, 'end')
         y = x[:-1]
-        self.resultwindow.insert(0, y)
+        self.result.insert(0, y)
 
     def calculate(self):
-        x = self.resultwindow.get()
+        x = self.result.get()
         ans = eval(x)
-        self.resultwindow.delete(0, 'end')
-        self.resultwindow.insert(0, ans)
+        self.result.delete(0, 'end')
+        self.result.insert(0, ans)
 
 if __name__ == "__main__":
     calc()
